d3c35d24a19b54a54dd7c2f85f33b6161322a57d
sheams <sheams.net@gmail.com>
2015-12-04 14:26:00 +0600

    p4 end

--- ConsoleProgram/Program.cs
+++ ConsoleProgram/Program.cs
@@ -270,7 +270,7 @@ namespace ConsoleProgram
 
 
     // Switch Statements program
-    class Program
+    /*class Program
     {
         static void Main(string[] args)
         {
@@ -296,6 +296,57 @@ namespace ConsoleProgram
                     break;
                 default:
                     Console.WriteLine("Not a leger Entry. please try again");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }*/
+
+
+
+
+
+    class Program
+    {
+        enum Color
+        {
+            Red,
+            Orange,
+            Green,
+            Blue,
+            Indigo,
+            Violet
+        }
+        static void Main(string[] args)
+        {
+            Color favorite = Color.Green;
+            switch(favorite)
+            {
+                case Color.Red:
+                    Console.WriteLine("You chose Red");
+                    break;
+
+                case Color.Violet:
+                    Console.WriteLine("You chose Violet");
+                    break;
+
+                case Color.Orange:
+                    Console.WriteLine("You chose Orange");
+                    break;
+
+                case Color.Indigo:
+                    Console.WriteLine("You chose Indigo");
+                    break;
+
+                case Color.Green:
+                    Console.WriteLine("You chose Green");
+                    break;
+
+                case Color.Blue:
+                    Console.WriteLine("You chose Blue");
+                    break;
+                default:
+                    Console.WriteLine("U didn't chose a color");
                     break;
             }
             Console.ReadKey();
